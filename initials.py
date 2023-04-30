e8238051bea8a2400a29d670cc2ad24138139bcc
KalaKenyon <100374253+KalaKenyon@users.noreply.github.com>
2023-04-29 23:27:30 -0400

    Added files
    Files created going through CodeAcademy.

---
+++ initials.py
@@ -0,0 +1,20 @@
+#Kala Kenyon
+#I was recruited by a hacker group 7 years ago.
+print("K   K K   K")
+print("K  K  K  K")
+print("K K   K K")
+print("KK    KK")
+print("K K   K K")
+print("K  K  K  K")
+print("K   K K   K")
+ 
+name = """
+K   K K   K
+K  K  K  K
+K K   K K
+KK    KK
+K K   K K
+K  K  K  K
+K   K K   K
+"""
+print(name)
---
+++ test.py
@@ -0,0 +1,1 @@
+print("Hey party people, I am restarting my learning and keeping track with github")
